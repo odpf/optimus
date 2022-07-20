a017827164e0c7a3ad13b80a79e19e7f3f795f73
Saikumar Chintada <chintadasaikumar@gmail.com>
2022-07-20 14:42:53 +0530

    add YamlPlugin

--- cmd/version/version.go
+++ cmd/version/version.go
@@ -110,6 +110,7 @@ func (v *versionCommand) printAllPluginInfos() {
 			v.logger.Info(fmt.Sprintf("Depends on: %v", schema.DependsOn))
 		}
 	}
+	models.YamlPluginRegistry.PrintAllPlugins(v.logger)
 }
 
 // getVersionRequest send a version request to service
